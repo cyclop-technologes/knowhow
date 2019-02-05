e7363b490f7f4ddf700c4107bd16a046664837b8
Ruzerio <ruzero.io@gmail.com>
2019-02-05 23:24:52 +0300

    second block

--- src/js/canvas.js
+++ src/js/canvas.js
@@ -1,102 +1,110 @@
-var canvas = document.getElementById("canvas"),
-  ctx = canvas.getContext('2d');
+module.exports = function (canvasID, starsAmount, color = '#000000', radius = 1, lineWidth = 0.05) {
+  var canvas = document.getElementById(canvasID),
+    ctx = canvas.getContext('2d');
 
-canvas.width = document.getElementById('canvas').offsetWidth;
-canvas.height = document.getElementById('canvas').offsetHeight;
+  canvas.width = document.getElementById(canvasID).offsetWidth;
+  canvas.height = document.getElementById(canvasID).offsetHeight;
 
-var stars = [], // Array that contains the stars
-  FPS = 60, // Frames per second
-  x = 50, // Number of stars
-  mouse = {
-    x: 0,
-    y: 0
-  }; // mouse location
+  var stars = [], // Array that contains the stars
+    FPS = 60, // Frames per second
+    x = starsAmount, // Number of stars
+    mouse = {
+      x: 0,
+      y: 0
+    }; // mouse location
 
-// Push stars to array
+  // Push stars to array
 
-for (var i = 0; i < x; i++) {
-  stars.push({
-    x: Math.random() * canvas.width,
-    y: Math.random() * canvas.height,
-    radius: Math.random() * 1 + 1,
-    vx: Math.floor(Math.random() * 50) - 25,
-    vy: Math.floor(Math.random() * 50) - 25
+  for (var i = 0; i < x; i++) {
+    stars.push({
+      x: Math.random() * canvas.width,
+      y: Math.random() * canvas.height,
+      radius: Math.random() * radius + radius,
+      vx: Math.floor(Math.random() * 50) - 25,
+      vy: Math.floor(Math.random() * 50) - 25
+    });
+  }
+
+  canvas.addEventListener('mousemove', function(e) {
+    mouse.x = e.clientX;
+    mouse.y = e.clientY;
   });
-}
 
-// Draw the scene
+  // Draw the scene
 
-function draw() {
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  function draw() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-  ctx.globalCompositeOperation = "lighter";
+    ctx.globalCompositeOperation = "lighter";
 
-  for (var i = 0, x = stars.length; i < x; i++) {
-    var s = stars[i];
+    for (var i = 0, x = stars.length; i < x; i++) {
+      var s = stars[i];
 
-    ctx.fillStyle = "#000000";
+      ctx.fillStyle = color;
+      ctx.beginPath();
+      ctx.arc(s.x, s.y, s.radius, 0, 2 * Math.PI);
+      ctx.fill();
+      ctx.fillStyle = color;
+      ctx.stroke();
+    }
+
     ctx.beginPath();
-    ctx.arc(s.x, s.y, s.radius, 0, 2 * Math.PI);
-    ctx.fill();
-    ctx.fillStyle = 'black';
+    for (var i = 0, x = stars.length; i < x; i++) {
+      var starI = stars[i];
+      ctx.moveTo(starI.x, starI.y);
+      if (distance(mouse, starI) < 150) ctx.lineTo(mouse.x, mouse.y);
+      for (var j = 0, x = stars.length; j < x; j++) {
+        var starII = stars[j];
+        if (distance(starI, starII) < 150) {
+          //ctx.globalAlpha = (1 / 150 * distance(starI, starII).toFixed(1));
+          ctx.lineTo(starII.x, starII.y);
+        }
+      }
+    }
+    ctx.lineWidth = lineWidth;
+    ctx.strokeStyle = color;
     ctx.stroke();
   }
 
-  ctx.beginPath();
-  for (var i = 0, x = stars.length; i < x; i++) {
-    var starI = stars[i];
-    ctx.moveTo(starI.x, starI.y);
-    if (distance(mouse, starI) < 150) ctx.lineTo(mouse.x, mouse.y);
-    for (var j = 0, x = stars.length; j < x; j++) {
-      var starII = stars[j];
-      if (distance(starI, starII) < 150) {
-        //ctx.globalAlpha = (1 / 150 * distance(starI, starII).toFixed(1));
-        ctx.lineTo(starII.x, starII.y);
-      }
+  function distance(point1, point2) {
+    var xs = 0;
+    var ys = 0;
+
+    xs = point2.x - point1.x;
+    xs = xs * xs;
+
+    ys = point2.y - point1.y;
+    ys = ys * ys;
+
+    return Math.sqrt(xs + ys);
+  }
+
+  // Update star locations
+
+  function update() {
+    for (var i = 0, x = stars.length; i < x; i++) {
+      var s = stars[i];
+
+      s.x += s.vx / FPS;
+      s.y += s.vy / FPS;
+
+      if (s.x < 0 || s.x > canvas.width) s.vx = -s.vx;
+      if (s.y < 0 || s.y > canvas.height) s.vy = -s.vy;
     }
   }
-  ctx.lineWidth = 0.05;
-  ctx.strokeStyle = 'black';
-  ctx.stroke();
-}
 
-function distance(point1, point2) {
-  var xs = 0;
-  var ys = 0;
 
-  xs = point2.x - point1.x;
-  xs = xs * xs;
 
-  ys = point2.y - point1.y;
-  ys = ys * ys;
+  // Update and draw
 
-  return Math.sqrt(xs + ys);
-}
-
-// Update star locations
-
-function update() {
-  for (var i = 0, x = stars.length; i < x; i++) {
-    var s = stars[i];
-
-    s.x += s.vx / FPS;
-    s.y += s.vy / FPS;
-
-    if (s.x < 0 || s.x > canvas.width) s.vx = -s.vx;
-    if (s.y < 0 || s.y > canvas.height) s.vy = -s.vy;
+  function tick() {
+    draw();
+    update();
+    requestAnimationFrame(tick);
   }
+
+
+  tick();
+  
 }
 
-canvas.addEventListener('mousemove', function(e) {
-  mouse.x = e.clientX;
-  mouse.y = e.clientY;
-});
-
-// Update and draw
-
-
-module.exports = function tick() {
-  draw();
-  update();
-  requestAnimationFrame(tick);
-}
--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,42 @@
-const canvas = require('./canvas.js');
+const сanvas = require('./canvas.js');
+const head = require('./head.js');
 const anime = require('animejs');
+const $ = require('jquery');
 
-canvas()
+
+$('#summary__bg').attr({
+	height: window.innerHeight,
+});
+
+const headBg = сanvas('canvas', 50);
+const summaryBg = сanvas('summary__bg', 200, '#d3d3d3', 2, 0.2);
+head();
+
+
+let $window = $(window);
+let $elem = $(".summary__top-title");
+
+
+
+function isScrolledIntoView($elem, $window) {
+    var docViewTop = $window.scrollTop();
+    var docViewBottom = docViewTop + $window.height();
+
+    var elemTop = $elem.offset().top;
+    var elemBottom = elemTop + $elem.height();
+
+    return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
+}
+
+
+$(document).on("scroll", function () {
+    if (isScrolledIntoView($elem, $window)) {
+
+    	$('.summary__top-title i').each(function(i, el){
+
+    		setTimeout(function(){
+    			el.classList.add('fill')
+    		}, i * 200)
+    	})
+    }
+});
