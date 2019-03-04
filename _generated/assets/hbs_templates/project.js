751292e14db0b7a41ea684080e8b535a42e5b7c7
Ruzerio <ruzero.io@gmail.com>
2019-03-04 23:25:15 +0300

    add some fields

--- _generated/assets/hbs_templates/project.js
+++ _generated/assets/hbs_templates/project.js
@@ -1,7 +1,7 @@
 define(["handlebars"], function(Handlebars) { return Handlebars.template({"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
     var stack1;
 
-  return "<section class=\"project\">\r\n	<div class=\"wrapper project__container\">\r\n		<div class=\"project__title\">анатомия вашей компании</div>\r\n\r\n"
+  return "<section class=\"project\">\n	<div class=\"wrapper project__container\">\n		<div class=\"project__title\">анатомия вашей компании</div>\n\n"
     + ((stack1 = container.invokePartial(partials["sculpture-img"],depth0,{"name":"sculpture-img","data":data,"indent":"\t\t","helpers":helpers,"partials":partials,"decorators":container.decorators})) != null ? stack1 : "")
-    + "\r\n	</div>\r\n\r\n	<footer class=\"project__footer\">\r\n		<img class=\"--bright-wave\" src=\"assets/img/wave.png\" alt=\"\">\r\n		<img class=\"--dark-wave\" src=\"assets/img/wave-dark.png\" alt=\"\">		\r\n	</footer>\r\n</section>";
+    + "\n	</div>\n\n	<footer class=\"project__footer\">\n		<img class=\"--bright-wave\" src=\"assets/img/wave.png\" alt=\"\">\n		<img class=\"--dark-wave\" src=\"assets/img/wave-dark.png\" alt=\"\">		\n	</footer>\n</section>";
 },"usePartial":true,"useData":true}); });
--- cms/index.js
+++ cms/index.js
@@ -1,6 +1,8 @@
 ({
 	header: {
 		header_title: 'knowhow',
+		description: '',
+		keywords: '',
 		to_form_btn: 'хочу шедевр',
 		media_links: [
 			{
@@ -19,39 +21,106 @@
 				name: 'facebook',
 				link: 'https://facebook.com'
 			}
-		]
+		],
+		email: 'hello@kh.agency'
 	},
 	forms: {
-		title: "Начать проект",
-		subtitle: 'СПАСИБО ЗА ПРОЯВЛЕННЫЙ ИНТЕРЕС К НАШЕЙ РАБОТЕ. ПОЖАЛУЙСТА, ЗАПОЛНИТЕ ФОРМУ НИЖЕ, И МЫ СВЯЖЕМСЯ С ВАМИ ДЛЯ УТОЧНЕНИЯ ДЕТАЛЕЙ В ТЕЧЕНИЕ ОДНОГО РАБОЧЕГО ДНЯ.',
-		branding: {
-			name: 'брендинг',
-			subitems: [
-				{
-					name :"ФИРМЕННЫЙ СТИЛЬ",
-					price: 20,
-				},
-				{
-					name :'ЛОГОТИП',
-					price: 20,
-				},
-				{
-					name :'ДИЗАЙН',
-					price: 20,
-				},
-				{
-					name :'БРЕНДБУК',
-					price: 20,
-				},
-				{
-					name :'НЕЙМИНГ',
-					price: 20,
-				},
-				{
-					name :'КОПИРАЙТИНГ',
-					price: 20,
-				},
-			]
-		}
+		title: 'Начать проект',
+		$subtitle_type: 'html',
+		subtitle: 'Cпасибо за проявленный интерес к нашей работе. \nПожалуйста, заполните форму ниже, и мы свяжемся с вами для уточнения деталей в течение одного рабочего дня.',
+		items: [
+			{
+				id: 'branding',
+				name: 'брендинг',
+				subitems: [
+					{
+						name: 'ФИРМЕННЫЙ СТИЛЬ',
+						price: 20
+					},
+					{
+						name: 'ЛОГОТИП',
+						price: 20
+					},
+					{
+						name: 'ДИЗАЙН',
+						price: 20
+					},
+					{
+						name: 'БРЕНДБУК',
+						price: 20
+					},
+					{
+						name: 'НЕЙМИНГ',
+						price: 20
+					},
+					{
+						name: 'КОПИРАЙТИНГ',
+						price: 20
+					}
+				]
+			},
+			{
+				id: 'prodaction',
+				name: 'продакшн',
+				subitems: [
+					{
+						name: 'ИНТЕГРАЦИИ',
+						price: 20
+					},
+					{
+						name: 'ПРОМОРОЛИКИ',
+						price: 20
+					},
+					{
+						name: 'ПОЛНОЦЕННЫЕ СЮЖЕТЫ',
+						price: 20
+					},
+					{
+						name: 'КАНАЛ ПОД КЛЮЧ',
+						price: 20
+					}
+				]
+			},
+			{
+				id: 'it',
+				name: 'it',
+				subitems: [
+					{
+						name: 'САЙТ',
+						price: 20
+					},
+					{
+						name: 'ПРИЛОЖЕНИЕ',
+						price: 20
+					},
+					{
+						name: 'CRM',
+						price: 20
+					}
+				]
+			},
+			{
+				id: 'marketing',
+				name: 'маркетинг',
+				subitems: [
+					{
+						name: 'SMM-ПРОДВИЖЕНИЕ',
+						price: 20
+					},
+					{
+						name: 'SEO-ПРОДВИЖЕНИЕ',
+						price: 20
+					},
+					{
+						name: 'КОНТЕНТ-МАРКЕТИНГ',
+						price: 20
+					},
+					{
+						name: 'КОНТЕКСТНАЯ РЕКЛАМА',
+						price: 20
+					}
+				]
+			}
+		]
 	}
 })
