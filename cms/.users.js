4aaeff754cbf1d0ffa844abf360bab2d54b94301
Ruzerio <ruzero.io@gmail.com>
2019-03-06 14:41:24 +0300

    del user

--- cms/.users.js
+++ cms/.users.js
@@ -1,11 +1,5 @@
 ({
 	users: [
-		{
-			username: 'admin',
-			tags: [],
-			salt: '0707144f366280bd50d9ad6764909460',
-			hash: 'f7e01f8b9acfab80aef83857840698aeb11986aa5b47c8e3149663932da15c87',
-			user_created_timestamp: 1550671675926
-		}
+		
 	]
 })
